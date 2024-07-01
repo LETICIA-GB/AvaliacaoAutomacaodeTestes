7e8d084de840d2ea567d222c452a344564bb1436
LETICIA-GB <146875822+LETICIA-GB@users.noreply.github.com>
2024-06-30 22:16:25 -0300

    config para testes

---
+++ src/pesquisa/pesquisa.js
@@ -0,0 +1,21 @@
+function pesquisarCampo(campos, pesquisa) {
+}
+
+const campos = [
+    {
+      nome: "Nome",
+      texto: "Julio de Lima",
+      identificador: "campoNome"
+    },
+    {
+      nome: "E-mail",
+      texto: "Eiam@juliodelima.com.br",
+      identificador: 2
+    },
+    {
+      nome: "Telefone",
+      texto: "+55 11 99999-9999",
+      identificador: "campoTelefone"
+    },
+   
+  ];
